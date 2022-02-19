229873acf438a9ee2aa1264de8c4da9f3331a27a
1nsanov <victorbezunov@gmail.com>
2022-02-19 14:39:27 +0200

    Добавьте файлы проекта.

---
+++ LabWotk9_Dictionary/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWotk9_Dictionary
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public DateTime DateProduction { get; set; }
+        public int DaysExpiration { get; set; }
+        public int Price { get; set; }
+
+        public Product(string name, DateTime dateProduction, int daysExpiration, int price)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            DateProduction = dateProduction;
+            DaysExpiration = daysExpiration;
+            Price = price;
+        }
+
+        public DateTime GetDateEnd()
+        {
+            return DateProduction.AddDays(DaysExpiration);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} | {DateProduction.ToString("d")} | Годен до: {GetDateEnd().ToString("d")} | {Price}$";
+        }
+    }
+}
---
+++ LabWotk9_Dictionary/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWotk9_Dictionary
+{
+    class Program
+    {
+        private static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            var listProducts = new List<Product>();
+            for (int i = 1; i <= 7; i++)
+            {
+                var name = "Product" + i;
+                var dateProduction = new DateTime(2022, 1, 25).AddDays(rnd.Next(-5, 5));
+                var dateExpiration = rnd.Next(3, 10);
+                var price = rnd.Next(10, 50);
+                listProducts.Add(new Product(name, dateProduction, dateExpiration, price));
+            }
+
+            Console.WriteLine("All info");
+            OutputList(listProducts);
+
+            var dictionatyProduct = new Dictionary<string, Product>();
+            foreach (var item in listProducts)
+            {
+                dictionatyProduct.Add(item.Name, item);
+            }
+
+
+            Console.WriteLine("Output Task");
+            var tempHashSet = new HashSet<int>();
+            foreach (var item in dictionatyProduct)
+            {
+                tempHashSet.Add(item.Value.DaysExpiration);
+            }
+            foreach (var value in tempHashSet)
+            {
+                var count = 0;
+                foreach (var item in dictionatyProduct)
+                {
+                    if (value == item.Value.DaysExpiration)
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine($"Товаров с ср. годн. {value} - {count}");
+            }
+
+            Console.WriteLine("Введите желаемый срок годности.");
+            var days = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Товары со сроком годности = {days}");
+            OutputList(dictionatyProduct.Values.Where(p => p.DaysExpiration == days));
+
+
+            Console.ReadLine();
+        }
+
+        private static void OutputList(IEnumerable<Product> list)
+        {
+            Console.WriteLine();
+            if (list.Count() > 0)
+            {
+                Console.WriteLine("__________________________________________________________________");
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("__________________________________________________________________");
+            }
+            else
+            {
+                Console.WriteLine("Список пуст.");
+            }
+            Console.WriteLine();
+        }
+    }
+}
